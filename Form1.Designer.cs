92db9f0561982a0b8d65d4b17cab4384303767c5
José Giovani Delgado Santos <77900593+Giovani-DS@users.noreply.github.com>
2022-03-22 15:04:51 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Assistente_Virtual___Orion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barraDeAudio = new System.Windows.Forms.ProgressBar();
+            this.labelText = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // barraDeAudio
+            // 
+            this.barraDeAudio.Location = new System.Drawing.Point(4, 306);
+            this.barraDeAudio.Maximum = 50;
+            this.barraDeAudio.Name = "barraDeAudio";
+            this.barraDeAudio.Size = new System.Drawing.Size(197, 36);
+            this.barraDeAudio.TabIndex = 0;
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.BackColor = System.Drawing.Color.Transparent;
+            this.labelText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelText.ForeColor = System.Drawing.Color.OrangeRed;
+            this.labelText.Location = new System.Drawing.Point(12, 9);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(0, 24);
+            this.labelText.TabIndex = 1;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelStatus.Location = new System.Drawing.Point(1, 437);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(720, 13);
+            this.labelStatus.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Assistente_Virtual.Properties.Resources.orion;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(733, 450);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.barraDeAudio);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "x";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar barraDeAudio;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ saidaSom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace Assistente_Virtual___Orion
+{
+    class saidaSom
+    {
+        private static SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        public static void Speak(string text)
+        {
+            if (synthesizer.State == SynthesizerState.Speaking)
+                synthesizer.SpeakAsyncCancelAll();
+
+            synthesizer.SpeakAsync(text);
+        }
+
+        public static void Speak(params string[] text)
+        {
+            Speak(text[new Random().Next(0, text.Length - 1)]);
+        }
+    }
+
+
+}
